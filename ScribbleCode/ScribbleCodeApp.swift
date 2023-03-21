5d77666603785e7dde38380772f7e436e8aaa566
kazuhiro hayashi <kazuhiro.hayashi.49@gmail.com>
2023-03-21 13:08:20 +0900

    Initial Commit

---
+++ ScribbleCode/ScribbleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleCodeApp.swift
+//  ScribbleCode
+//
+//  Created by 林和弘 on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
